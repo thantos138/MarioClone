c7a937905b10aa021c84eaf9128591d7754ea961
thantos138 <bssoohoo@uci.edu>
2017-08-30 11:46:39 -0700

    added camera script

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour
+{
+	public Transform Player;
+	public float distanceFromPlayer;
+	public float staticCameraY;
+
+	void Update()
+	{
+		gameObject.transform.position = new Vector3(Player.position.x, staticCameraY, Player.position.z - distanceFromPlayer);
+	}
+
+	void LateUpdate()
+	{
+		//GetComponent<Camera.main>().transform.position = staticCameraY;
+	} 
+}
